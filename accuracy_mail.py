469c6555279dd52943eb516fb86c9daec88f286d
SHUBHAM MEHTA <shhhhubhammmm@gmail.com>
2020-05-29 00:30:07 +0530

    first

---
+++ accuracy_mail.py
@@ -0,0 +1,9 @@
+with open("/root/accuracy.txt", "r") as f:
+ acc=f.read()
+import smtplib as sm
+msg="The model has been successfully created with the desired accuracy!! \nAccuracy of the model is: "+str(acc)+" percentage"
+server=sm.SMTP_SSL("smtp.gmail.com", 465)
+server.login("shhhhubhammmm@gmail.com","bpxwivpmgmkdgghn")
+server.sendmail("shhhhubhammmm@gmail.com","shhhhubhammmm@gmail.com", msg)
+server.quit()
+
