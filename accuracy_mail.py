e50618f22686dee00ccb65adfe4648ee1a3cfb45
devilsm13 <45040045+devilsm13@users.noreply.github.com>
2020-05-30 13:35:23 +0530

    kjvgh

--- accuracy_mail.py
+++
@@ -1,9 +0,0 @@
-with open("/root/accuracy.txt", "r") as f:
- acc=f.read()
-import smtplib as sm
-msg="The model has been successfully created with the desired accuracy!! \nAccuracy of the model is: "+str(acc)+" percentage"
-server=sm.SMTP_SSL("smtp.gmail.com", 465)
-server.login("shhhhubhammmm@gmail.com","bpxwivpmgmkdgghn")
-server.sendmail("shhhhubhammmm@gmail.com","shhhhubhammmm@gmail.com", msg)
-server.quit()
-
