5cb20597a83097e266ea085f0bf889c8e3d15ce5
devilsm13 <45040045+devilsm13@users.noreply.github.com>
2020-05-30 13:36:06 +0530

    nkh

--- error_mail.py
+++
@@ -1,6 +0,0 @@
-import smtplib as sm
-msg="Unfortunately, the container has been stopped working!!\nHowever, it has been started once again."
-server=sm.SMTP_SSL("smtp.gmail.com", 465)
-server.login("shhhhubhammmm@gmail.com","bpxwivpmgmkdgghn")
-server.sendmail("shhhhubhammmm@gmail.com","shhhhubhammmm@gmail.com", msg)
-server.quit()
